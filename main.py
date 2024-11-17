eb63f75ce681b25ea0d6cadf52044a245cc861b0
tjards <travis.jardine@gmail.com>
2024-11-17 12:52:53 -0500

    ready for 2D option build

--- main.py
+++ main.py
@@ -58,12 +58,12 @@ data_file_path = os.path.join(data_directory, "data.h5")
 np.random.seed(42)
 
 Ti      = 0       # initial time
-Tf      = 30     # final time (later, add a condition to break out when desirable conditions are met)
+Tf      = 10     # final time (later, add a condition to break out when desirable conditions are met)
 Ts      = 0.02    # sample time
 f       = 0       # parameter for future use
 verbose = 1       # 1 = print progress reports, 0 = silent
 system   = 'swarm' 
-strategy = 'cao'
+strategy = 'saber'
 
     # reynolds  = Reynolds flocking + Olfati-Saber obstacle
     # saber     = Olfati-Saber flocking
