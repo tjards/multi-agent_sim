ec11c6b3aeeb70c9f06a83648e1155b7fc06dbe7
tjards <travis.jardine@gmail.com>
2025-04-06 21:38:16 -0400

    cleaned up umap code

--- data/data_for_UMAP.py
+++ data/data_for_UMAP.py
@@ -5,14 +5,23 @@ Created on Sat Apr  5 21:09:01 2025
 
 @author: tjards
 
-Show swarm dynamics lie on a circular manifold in latent space.
-
-You’ve reconstructed the phase of your system without explicitly giving it to the model.
+This project uses UMAP (https://github.com/lmcinnes/umap) 
+    to demonstgrate the swarm dynamics outlined in 
+    "Emergent Homeomorphic Curves in Swarms", Automatica, 2025
+    (https://doi.org/10.1016/j.automatica.2025.112221)
+    lie on a circular manifold in latent space.
+    
+    We reconstruct the phase of the system without explicity giving
+    it to the model; this essentially "discovers" the circular
+    embedding formulated in the paper using data-based methods.
+    
+    Accepts agent data produced from simimlations at 
+    https://github.com/tjards/multi-agent_sim
 
 """
 
-# import stuff
-# ------------
+#%% preliminaries
+# ---------------
 import os
 import data_manager
 import pandas as pd
@@ -20,26 +29,22 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 # data paths
-# -----------
 data_directory = 'data'
 data_file_path = os.path.join(data_directory, "data.h5")
 
 # labels
-# ------
 labels = ['x','y','z','vx','vy','vz','cmdx', 'cmdy','cmdz']
 
-# extract values
-# --------------
+#%% extract, transform, load data
+# -------------------------------
 states_all_keys,    states_all_values   = data_manager.load_data_HDF5('History', 'states_all', data_file_path)
 cmds_all_keys,      cmds_all_values     = data_manager.load_data_HDF5('History', 'cmds_all', data_file_path)
 
 # create a new dataframe
-# ----------------------
 agent_data = {}
 nAgents = states_all_values.shape[2]
 
 # for each agent
-# --------------
 t_start = 100
 t_end = states_all_values.shape[0]+1
 for i in range(nAgents):
@@ -56,11 +61,9 @@ for i in range(nAgents):
     agent_data[labels[8]+'_'+str(i)] = cmds_all_values[t_start:t_end,2,i]
         
 # make a dataframe
-# ----------------
 df = pd.DataFrame(agent_data)
 
 # prep data
-# ---------
 window_size = 200
 stride = 1
 
@@ -83,52 +86,53 @@ X_flat = X.reshape(X.shape[0], -1)
 X_scaled = StandardScaler().fit_transform(X_flat)
 
 
-#%% do the UMAP stuff
+#%% model using UMAP
 # -------------------
 UMAP_para= False
 
+# is using basic UMAP
 if not UMAP_para:
     
-
     from umap import UMAP
-    #reducer = UMAP(n_neighbors=15, min_dist=0.1)
-    reducer = UMAP(n_neighbors=2, min_dist=0.9, n_components=2)
-    #reducer = UMAP(n_neighbors=10, min_dist=0.05, n_components=2)
 
+    # note: I use atypical UMAP parameters, which reflect the application
+    # I choose a small n_neighbours, indicating the reliance on local 
+    # observations described in the paper; yet, I use a high min_distance,
+    # indicating I care about the global behavious
+
+    # initialize, fit, transform
+    reducer = UMAP(n_neighbors=2, min_dist=0.9, n_components=2) # 
     X_umap = reducer.fit_transform(X_scaled)
     
-    # extact useful
+    # extact phase info
     X_center = X_umap.mean(axis=0)
     theta = np.arctan2(X_umap[:, 1] - X_center[1], X_umap[:, 0] - X_center[0])
 
-    # plot stuff
+    #%% plot stuff
     import matplotlib.pyplot as plt
     
-    # plot type (time, theta)
     plot_type = 'theta'
-    
     plt.figure(figsize=(8, 6))
     
+    # plots over time
     if plot_type == 'time':
         plt.scatter(X_umap[:,0], X_umap[:,1], c=np.arange(len(X_umap)), cmap='plasma', s=5)
-        plt.colorbar(label='Time Index')
-
+        plt.colorbar(label='Time Index (s)')
+    # plots over phase
     if plot_type == 'theta':
         plt.scatter(X_umap[:,0], X_umap[:,1], c=theta, cmap='plasma', s=5)
-        plt.colorbar(label='Angular Index')
+        plt.colorbar(label='Phase Index (radians)')
     
     plt.title("UMAP of Swarm Dynamics")
     plt.grid(True)
     plt.show()
 
+# if using parametric (useful for later ML application)
 else:
     
-    #conda install -c conda-forge umap-learn tensorflow
-    #import umap.umap_ as umap
-    #from tensorflow.keras.callbacks import EarlyStopping
     from umap.parametric_umap import ParametricUMAP
 
-    # initialize para UMAP (uses a simple MLP by default)
+    # initialize
     pumap = ParametricUMAP(
         n_neighbors=2,
         min_dist=0.02, # default 0.1
@@ -145,31 +149,18 @@ else:
     #    restore_best_weights=True
     #)
     
-    # fit to your windowed time series data
+    # fit, transform
     #X_pumap = pumap.fit_transform(X_scaled, callbacks=[early_stop])
     X_pumap = pumap.fit_transform(X_scaled)
     
-    #%%
+    #%% plot stuff
     import matplotlib.pyplot as plt
-
     plt.figure(figsize=(8, 6))
     plt.scatter(X_pumap[:, 0], X_pumap[:, 1], c=np.arange(len(X_pumap)), cmap='plasma', s=5)
     plt.colorbar(label='Time Index')
-    plt.title("Parametric UMAP Embedding of Agent Trajectory Windows")
+    plt.title("UMAP of Swarm Dynamics (parametric)")
     plt.grid(True)
     plt.show()
-
-#%% add a circle
-# ---------------
-# estimate center and radius
-#x_mean = np.mean(X_umap[:, 0])
-#y_mean = np.mean(X_umap[:, 1])
-#center = np.array([x_mean, y_mean])
-#r = np.mean(np.linalg.norm(X_umap - center, axis=1))
-#theta = np.linspace(0, 2 * np.pi, 500)
-#circle_x = center[0] + r * np.cos(theta)
-#circle_y = center[1] + r * np.sin(theta)
-#plt.plot(circle_x, circle_y, 'k--', label='Fitted Circle')
 
 #%% draw a convex hull
 # --------------------
